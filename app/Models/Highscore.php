cdc35e013321be5770cb8f5b51766120455ad3d5
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-05 16:48:17 +0200

    Update model

--- app/Models/Highscore.php
+++ app/Models/Highscore.php
@@ -9,6 +9,13 @@ class Highscore extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nickname', 'score', 'source', 'project_id'];
+
     public function project()
     {
         return $this->hasOne(Project::class);
