577e1872c5da69f98c1980db56fcdce071a820a8
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-06 16:13:53 +0200

    Fix PHP path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,7 @@ chmod +x ${COMPOSER_PATH}
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+(${PHP_PATH} artisan down) || true
 
 # Pull the latest version of the app
 git pull origin production
